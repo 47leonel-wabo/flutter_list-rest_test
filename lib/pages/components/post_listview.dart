9a84baca94bd1233aa07d2eb3672227c7a4967ca
leo-DreamcashPC <waboleonel@gmail.com>
2020-10-08 10:43:57 +0100

    Adding onTap function and SnackBar

--- lib/pages/components/post_listview.dart
+++ lib/pages/components/post_listview.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:rest_test/model/user.dart';
 
@@ -6,8 +8,14 @@ class ListViewData extends StatelessWidget {
 
   ListViewData(Key key, this.posts) : super(key: key);
 
+  void _onTapItem(BuildContext context, NewsPost newsPost){
+    Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text(newsPost.id.toString()+' '+ newsPost.title)));
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Card(
       child: ListView.builder(
         itemCount: posts.length,
@@ -19,31 +27,36 @@ class ListViewData extends StatelessWidget {
                 height: 10.0,
               ),
               ListTile(
-                  title: Text(
-                    '${posts[position].title}',
-                    style: TextStyle(
-                        fontSize: 20.0, color: Colors.deepOrangeAccent),
+                onTap: () {
+                  _onTapItem(context, posts[position]);
+                },
+                title: Text(
+                  '${posts[position].title}',
+                  style:
+                      TextStyle(fontSize: 20.0, color: Colors.deepOrangeAccent),
+                ),
+                subtitle: Text(
+                  '${posts[position].body}',
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 15.0,
+                    color: Colors.grey[850],
                   ),
-                  subtitle: Text(
-                    '${posts[position].body}',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Colors.grey[850],
-                    ),
-                  ),
-                  leading: Column(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.blueAccent,
-                        radius: 28.0,
-                        child: Text(
-                          'User ${posts[position].userId}',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 15.0, color: Colors.white),
-                        ),
+                ),
+                leading: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.blueAccent,
+                      radius: 28.0,
+                      child: Text(
+                        'User ${posts[position].userId}',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15.0, color: Colors.white),
                       ),
-                    ],
-                  )),
+                    ),
+                  ],
+                ),
+              ),
             ],
           );
         },
